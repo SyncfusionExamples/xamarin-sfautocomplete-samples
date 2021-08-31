3c80c570a3e7e3c89968c3f08dd85bffcf0d8f07
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-08-31 12:31:26 +0530

    Add files via upload

---
+++ How-to-show-the-dropdown-even-typed-text-is-not-available-in-DataSource/AutoCompleteFilter/AutoCompleteFilter/AutoCompleteFilter/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AutoCompleteFilter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            autoComplete.Filter = ContainingSpaceFilter;
+        }
+
+        public bool ContainingSpaceFilter(string search, object item)
+        {
+            return true;
+        }
+    }
+}
---
+++ How-to-show-the-dropdown-even-typed-text-is-not-available-in-DataSource/AutoCompleteFilter/AutoCompleteFilter/AutoCompleteFilter/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AutoCompleteFilter
+{
+    public class EmployeeViewModel
+    {
+        private ObservableCollection<Employee> employeeCollection;
+        public ObservableCollection<Employee> EmployeeCollection
+        {
+            get { return employeeCollection; }
+            set { employeeCollection = value; }
+        }
+        public EmployeeViewModel()
+        {
+            employeeCollection = new ObservableCollection<Employee>();
+            employeeCollection.Add(new Employee() { ID = 1, Name = "Eric" });
+            employeeCollection.Add(new Employee() { ID = 2, Name = "James" });
+            employeeCollection.Add(new Employee() { ID = 3, Name = "Jacob" });
+            employeeCollection.Add(new Employee() { ID = 4, Name = "Lucas" });
+            employeeCollection.Add(new Employee() { ID = 5, Name = "Mark" });
+            employeeCollection.Add(new Employee() { ID = 6, Name = "Aldan" });
+            employeeCollection.Add(new Employee() { ID = 7, Name = "Aldrin" });
+            employeeCollection.Add(new Employee() { ID = 8, Name = "Alan" });
+            employeeCollection.Add(new Employee() { ID = 9, Name = "Aaron" });
+        }
+    }
+}
